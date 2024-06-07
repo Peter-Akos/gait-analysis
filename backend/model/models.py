3562975759f143f2bf7daf4d62f1c7870c146f9c
akos.peter <peterakos19@gmail.com>
2024-06-07 16:26:32 +0300

    refactor: small changes

--- backend/model/models.py
+++ backend/model/models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class GaitAnalysisModel(nn.Module):
@@ -9,16 +10,82 @@ class GaitAnalysisModel(nn.Module):
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.dropout = nn.Dropout(dropout_prob)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.fc1 = nn.Linear(hidden_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-
         out, _ = self.lstm(x, (h0, c0))
-
         last_output = out[:, -1, :]
         out = self.dropout(last_output)
-
-        out = self.fc(out)
+        out = F.relu(self.fc1(out))
+        out = self.fc2(out)
         return out
+
+
+class CNN(nn.Module):
+    def __init__(self, input_dim=500, vid_length=16, conv_dim=32, filter_length=8, dropout_amount=0.5,
+                 l2_lambda=10 ** (-3.5),
+                 last_layer_dim=10):
+        super(CNN, self).__init__()
+
+        # Define layers
+        self.conv1 = nn.Conv1d(in_channels=input_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn1 = nn.BatchNorm1d(conv_dim)
+
+        self.conv2 = nn.Conv1d(in_channels=conv_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn2 = nn.BatchNorm1d(conv_dim)
+
+        self.pool1 = nn.MaxPool1d(kernel_size=2)
+        self.drop1 = nn.Dropout(dropout_amount)
+
+        self.conv3 = nn.Conv1d(in_channels=conv_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn3 = nn.BatchNorm1d(conv_dim)
+
+        self.conv4 = nn.Conv1d(in_channels=conv_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn4 = nn.BatchNorm1d(conv_dim)
+
+        self.pool2 = nn.MaxPool1d(kernel_size=2)
+        self.drop2 = nn.Dropout(dropout_amount)
+
+        self.conv5 = nn.Conv1d(in_channels=conv_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn5 = nn.BatchNorm1d(conv_dim)
+
+        self.conv6 = nn.Conv1d(in_channels=conv_dim, out_channels=conv_dim, kernel_size=filter_length, padding='same')
+        self.bn6 = nn.BatchNorm1d(conv_dim)
+
+        self.pool3 = nn.MaxPool1d(kernel_size=3)
+        self.drop3 = nn.Dropout(dropout_amount)
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(conv_dim * (vid_length // 12),
+                             last_layer_dim)  # Calculate the output size after convolutions and pooling
+        self.fc2 = nn.Linear(last_layer_dim, 2)
+
+        # Apply L2 regularization to conv layers
+        for layer in [self.conv3, self.conv4, self.conv5, self.conv6]:
+            layer.weight = nn.Parameter(layer.weight + l2_lambda * layer.weight)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool1(x)
+        x = self.drop1(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool2(x)
+        x = self.drop2(x)
+
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.pool3(x)
+        x = self.drop3(x)
+
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x
--- backend/train.py
+++ backend/train.py
@@ -1,35 +1,78 @@
 import pickle
 import pandas as pd
 import torch
+from sklearn.preprocessing import StandardScaler
 from torch import nn, optim
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 import wandb
 
-
-from model.models import GaitAnalysisModel
+from model.models import GaitAnalysisModel, CNN
 
 X_FILENAME = "data/X_100.pkl"
 SPLIT_PATH = "data/split.pkl"
 Y_FILENAME = "data/y.csv"
-PREDICTED_VALUE = "speed"
+PREDICTED_VALUE = "cadence"
 BATCH_SIZE = 8
-NR_EPOCHS = 50
-DROPOUT_PROB = 0.5
-HIDDEN_SIZE = 128
+NR_EPOCHS = 10
+OPTIMIZER = "RMSprop"
+LEARNING_RATE = 0.001
+
+MODEL_ARCHITECTURE = "CNN"
+
+# CONV1D PARAMETERS
+INPUT_DIM = 500
+VID_LENGTH = 16
+CONV_DIM = 32
+FILTER_LENGTH = 8
+DROPOUT_AMOUNT = 0.5
+L2_LAMBDA = 10 ** (-3.5)
+LAST_LAYER_DIM = 10
+
+# LSTM PARAMETERS
 NUM_LSTM_LAYERS = 2
-MODEL_ARCHITECTURE = "LSTM"
+HIDDEN_SIZE = 128
+DROPOUT_PROB = 0.5
+
+hyperparameter_defaults = dict(
+    # All
+    nr_epochs=NR_EPOCHS,
+    learning_rate=LEARNING_RATE,
+    model_architecture=MODEL_ARCHITECTURE,
+    batch_size=BATCH_SIZE,
+    x_filename=X_FILENAME,
+    y_filename=Y_FILENAME,
+    split_path=SPLIT_PATH,
+    predicted_value=PREDICTED_VALUE,
+    optimizer=OPTIMIZER,
+    # LSTM
+    num_lstm_layers=NUM_LSTM_LAYERS,
+    hidden_size=HIDDEN_SIZE,
+    dropout_prob=DROPOUT_PROB,
+    # CNN
+    input_dim=INPUT_DIM,
+    vid_length=VID_LENGTH,
+    conv_dim=CONV_DIM,
+    filter_length=FILTER_LENGTH,
+    dropout_amount=DROPOUT_AMOUNT,
+    l2_lambda=L2_LAMBDA,
+    last_layer_dim=LAST_LAYER_DIM
+)
+
+wandb.init(config=hyperparameter_defaults, project="gait-analysis")
+config = wandb.config
 
 
 def get_split(X, y_df, video_ids):
     res_X = []
     res_y = []
+    scaler = StandardScaler()
     for video_id, X_data in X:
         if video_id in video_ids:
-            curr_X = X_data
-            curr_y = y_df[y_df["video_id"] == int(video_id)][PREDICTED_VALUE].tolist()
-            if len(curr_y) == 2 and len(curr_X) == 500:
-                res_X.append(X_data)
+            scaled_X = scaler.fit_transform(X_data)
+            curr_y = y_df[y_df["video_id"] == int(video_id)].sort_values(by=['side'])[PREDICTED_VALUE].tolist()
+            if len(curr_y) == 2 and len(scaled_X) == 500:
+                res_X.append(scaled_X)
                 res_y.append(curr_y)
 
     X_numpy = np.array(res_X)
@@ -62,36 +105,32 @@ def prepare_data(test_only=False):
     return train_loader, validation_loader, test_loader, input_size
 
 
-def setup_wandb():
-    wandb.init(
-        # set the wandb project where this run will be logged
-        project="gait-analysis",
-
-        # track hyperparameters and run metadata
-        config={
-            "predicted_value": PREDICTED_VALUE,
-            "architecture": MODEL_ARCHITECTURE,
-            "dataset": X_FILENAME,
-            "epochs": NR_EPOCHS,
-        }
-    )
-
-
 def train():
     train_loader, validation_loader, test_loader, input_size = prepare_data()
-    model = GaitAnalysisModel(input_size, HIDDEN_SIZE, NUM_LSTM_LAYERS, 2, DROPOUT_PROB)
+    if config.model_architecture == "LSTM":
+        model = GaitAnalysisModel(input_size, config.hidden_size, config.num_lstm_layers, 2, config.dropout_prob)
+    elif config.model_architecture == "CNN":
+        model = CNN(
+            input_dim=config.input_dim,
+            vid_length=config.vid_length,
+            conv_dim=config.conv_dim,
+            filter_length=config.filter_length,
+            dropout_amount=config.dropout_amount,
+            l2_lambda=config.l2_lambda,
+            last_layer_dim=config.last_layer_dim
+        )
 
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     best_model_state_dict = None
     best_val_loss = float('inf')
-
-    setup_wandb()
 
     print("Starting training...")
 
     # Training loop
-    for epoch in range(NR_EPOCHS):
+    for epoch in range(config.nr_epochs):
         model.train()  # Set the model to training mode
         running_loss = 0.0
         for inputs, labels in train_loader:
@@ -105,7 +144,7 @@ def train():
             running_loss += loss.item() * inputs.size(0)
 
         epoch_loss = running_loss / len(train_loader.dataset)
-        print(f'Epoch {epoch + 1}/{NR_EPOCHS}, Loss: {epoch_loss:.4f}')
+        print(f'Epoch {epoch + 1}/{config.nr_epochs}, Loss: {epoch_loss:.4f}')
 
         # Validation step
         model.eval()  # Set the model to evaluation mode
@@ -121,6 +160,7 @@ def train():
             best_val_loss = val_loss
             best_model_state_dict = model.state_dict()
             print("New best model found!")
+        scheduler.step()
         print(f'Validation Loss: {val_loss:.4f}')
 
         wandb.log({"train_loss": epoch_loss, "validation_loss": val_loss})
@@ -139,8 +179,8 @@ def train():
 
     test_loss = test_loss / len(test_loader.dataset)
     print(f'Test Loss: {test_loss:.4f}')
-    torch.save(model, f"models/{MODEL_ARCHITECTURE}_{NR_EPOCHS}_{test_loss}.pth")
-    wandb.log({"test_loss": test_loss})
+    torch.save(best_model_state_dict, f"models/{config.model_architecture}_{config.nr_epochs}_{test_loss}.pth")
+    wandb.log({"test_loss": test_loss, "best_validation_loss": best_val_loss})
 
 
 if __name__ == "__main__":
